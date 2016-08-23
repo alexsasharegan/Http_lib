9ebb39cc0f7a9118de9bc3a6508b312cd92a3d4b
Alex Regan <alex.joseph.regan@gmail.com>
2016-08-23 00:39:40 -0700

    init commit

---
+++ Http_Autoloader.php
@@ -0,0 +1,11 @@
+<?php
+function Http_Autoloader($classname) {
+  $pathParts = explode("\\", $classname);
+  array_unshift($pathParts, 'lib');
+  $path = implode(DIRECTORY_SEPARATOR, $pathParts);
+  $filename = __DIR__.DIRECTORY_SEPARATOR.$path.'.class.php';
+  if (is_readable($filename)) {
+    require_once $filename;
+  }
+}
+spl_autoload_register('Http_Autoloader', true);
---
+++ lib/Http.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class Http {
+
+  var $method, $req, $res, $url;
+
+  function __construct() {
+    header('Content-Type: application/json');
+    date_default_timezone_set("America/Phoenix");
+    $this->method = $_SERVER['REQUEST_METHOD'];
+    $this->req = json_decode(file_get_contents('php://input'),TRUE);
+    $this->url = $_SERVER['REQUEST_URI'];
+  }
+
+  public function sendJSON($statusCode = 200) {
+    http_response_code($statusCode);
+    $json = json_encode($this->$res);
+    echo $data;
+    exit;
+  }
+
+}
