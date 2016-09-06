12498a962c1e3b034914b16a3b5c5e7b860e33b6
Alex Regan <alex.regan@artisancolour.com>
2016-09-05 21:14:35 -0700

    switched to using 'src' directory

--- Http_Autoloader.php
+++ Http_Autoloader.php
@@ -1,11 +1,13 @@
 <?php
+
 function Http_Autoloader($classname) {
   $pathParts = explode("\\", $classname);
-  array_unshift($pathParts, 'lib');
+  array_unshift($pathParts, 'src');
   $path = implode(DIRECTORY_SEPARATOR, $pathParts);
   $filename = __DIR__.DIRECTORY_SEPARATOR.$path.'.class.php';
   if (is_readable($filename)) {
     require_once $filename;
   }
 }
+
 spl_autoload_register('Http_Autoloader', true);
