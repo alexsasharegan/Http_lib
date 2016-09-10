3bde2b6f01f84de07184285d42d972b41bbea0f8
Alex Regan <alex.regan@artisancolour.com>
2016-09-10 09:57:56 -0700

    added PHPDoc blocks

--- src/Http.php
+++ src/Http.php
@@ -6,11 +6,19 @@ use Http\Exceptions\InvalidStatusCode;
 
 class Http {
 	
+	/**
+	 * @param string $url
+	 */
 	public static function redirect( $url = '/' ) {
 		header( "Location: $url" );
 		exit;
 	}
 	
+	/**
+	 * @param int $statusCode
+	 * @return int
+	 * @throws InvalidStatusCode
+	 */
 	public static function status( $statusCode ) {
 		if ( !empty($statusCode) ):
 			
@@ -27,6 +35,9 @@ class Http {
 		endif;
 	}
 	
+	/**
+	 * Http constructor.
+	 */
 	function __construct() {
 		date_default_timezone_set( "America/Phoenix" );
 		set_exception_handler( [ $this, 'handleError' ] );
@@ -47,6 +58,9 @@ class Http {
 		$this->response = new Response;
 	}
 	
+	/**
+	 *
+	 */
 	private function handleDefault() {
 		$this->response->set_array( [
 			'error' => [
@@ -56,6 +70,10 @@ class Http {
 		$this->send( Response::HTTP_METHOD_NOT_ALLOWED );
 	}
 	
+	/**
+	 * @param callable $cb
+	 * @return Http $this
+	 */
 	public function get( callable $cb ) {
 		$args            = array_slice( func_get_args(), 1 );
 		$this->GETParams = $args;
@@ -63,6 +81,10 @@ class Http {
 		return $this;
 	}
 	
+	/**
+	 * @param callable $cb
+	 * @return Http $this
+	 */
 	public function post( callable $cb ) {
 		$args             = array_slice( func_get_args(), 1 );
 		$this->POSTParams = $args;
@@ -70,6 +92,10 @@ class Http {
 		return $this;
 	}
 	
+	/**
+	 * @param callable $cb
+	 * @return Http $this
+	 */
 	public function put( callable $cb ) {
 		$args            = array_slice( func_get_args(), 1 );
 		$this->PUTParams = $args;
@@ -77,6 +103,10 @@ class Http {
 		return $this;
 	}
 	
+	/**
+	 * @param callable $cb
+	 * @return Http $this
+	 */
 	public function patch( callable $cb ) {
 		$args              = array_slice( func_get_args(), 1 );
 		$this->PATCHParams = $args;
@@ -84,6 +114,10 @@ class Http {
 		return $this;
 	}
 	
+	/**
+	 * @param callable $cb
+	 * @return Http $this
+	 */
 	public function delete( callable $cb ) {
 		$args               = array_slice( func_get_args(), 1 );
 		$this->DELETEParams = $args;
@@ -91,11 +125,18 @@ class Http {
 		return $this;
 	}
 	
+	/**
+	 * @param callable $cb
+	 * @return Http $this
+	 */
 	public function error( callable $cb ) {
 		set_exception_handler( $cb );
 		return $this;
 	}
 	
+	/**
+	 *
+	 */
 	public function exec() {
 		$method = $this->request->method;
 		
@@ -107,6 +148,12 @@ class Http {
 		endif;
 	}
 	
+	/**
+	 * @param int $statusCode
+	 * @param string $contentType
+	 * @param string $content
+	 * @throws InvalidStatusCode
+	 */
 	public function send( $statusCode = 200, $contentType = 'application/json', $content = '' ) {
 		header( "Content-Type: $contentType; charset=UTF-8" );
 		
@@ -127,14 +174,24 @@ class Http {
 	
 	# this is really an exception handler
 	# error is just syntactical sugar
+	/**
+	 * @param \Exception $e
+	 */
 	public function handleError( \Exception $e ) {
 		$this->send( 500, 'application/json', json_encode( [ 'error' => $e ] ) );
 	}
 	
+	/**
+	 * @param int $statusCode
+	 * @param string $message
+	 */
 	public function abort( $statusCode = 404, $message = 'Sorry, something went wrong.' ) {
 		$this->send( $statusCode, 'application/json', json_encode( [ 'message' => $message ] ) );
 	}
 	
+	/**
+	 * @return string
+	 */
 	public function __toString() {
 		return json_encode( $this );
 	}
--- src/Request.php
+++ src/Request.php
@@ -3,60 +3,74 @@
 namespace Http;
 
 class Request {
-
-  private $body,
-          $_query;
-
-  function __construct() {
-
-    $this->contentType = isset( $_SERVER['CONTENT_TYPE'] ) ? $_SERVER['CONTENT_TYPE'] : '';
-    $this->cookies = isset( $_SERVER['HTTP_COOKIE'] ) ? $_SERVER['HTTP_COOKIE'] : '';;
-
-    // PHP throws a scrict-mode warning if we don't use an interim variable
-    // "Strict Standards: Only variables should be passed by reference"
-    $scriptNameArray = explode( '/', $_SERVER['SCRIPT_NAME'] );
-    $this->file = end( $scriptNameArray );
-
-    $this->host = $_SERVER['HTTP_HOST'];
-    $this->method = $_SERVER['REQUEST_METHOD'];
-    $this->_query = $_GET;
-    $this->port = $_SERVER['SERVER_PORT'];
-    $this->pathInfo = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
-    $this->requestURI = $_SERVER['REQUEST_URI'];
-    $this->scriptName = $_SERVER['SCRIPT_NAME'];
-    $this->URIComponents = parse_url($_SERVER['REQUEST_URI']);
-    $this->userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-    switch ( strtolower( $this->contentType ) ) {
-      case stristr($this->contentType, 'application/json'):
-        $this->body = json_decode( file_get_contents('php://input'), true );
-        break;
-      case stristr($this->contentType, 'application/x-www-form-urlencoded'):
-        $this->body = $_POST;
-        break;
-      case stristr($this->contentType, 'text/plain'):
-        $this->body = file_get_contents( 'php://input' );
-        break;
-      default:
-        $this->body = @file_get_contents( 'php://input' );
-        break;
-    } # end switch
-
-  } # end constructor
-
-  public function get( $key = '' ) {
-    if ( !is_array( $this->body ) || empty( $key ) ) {
-      return isset( $this->body ) ? $this->body : null;
-    }
-    return isset( $this->body[$key] ) ? $this->body[$key] : null;
-  }
-
-  public function query( $key ) {
-    return isset( $this->_query[$key] ) ? $this->_query[$key] : null;
-  }
-
-  public function __toString() {
-    return json_encode( $this );
-  }
-
+	
+	private $_body,
+		$_query;
+	
+	/**
+	 * Request constructor.
+	 */
+	function __construct() {
+		
+		$this->contentType = isset($_SERVER['CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : '';
+		$this->cookies     = isset($_SERVER['HTTP_COOKIE']) ? $_SERVER['HTTP_COOKIE'] : '';;
+		
+		// PHP throws a scrict-mode warning if we don't use an interim variable
+		// "Strict Standards: Only variables should be passed by reference"
+		$scriptNameArray = explode( '/', $_SERVER['SCRIPT_NAME'] );
+		$this->file      = end( $scriptNameArray );
+		
+		$this->host          = $_SERVER['HTTP_HOST'];
+		$this->method        = $_SERVER['REQUEST_METHOD'];
+		$this->_query        = $_GET;
+		$this->port          = $_SERVER['SERVER_PORT'];
+		$this->pathInfo      = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+		$this->requestURI    = $_SERVER['REQUEST_URI'];
+		$this->scriptName    = $_SERVER['SCRIPT_NAME'];
+		$this->URIComponents = parse_url( $_SERVER['REQUEST_URI'] );
+		$this->userAgent     = $_SERVER['HTTP_USER_AGENT'];
+		
+		switch ( strtolower( $this->contentType ) ) {
+			case stristr( $this->contentType, 'application/json' ):
+				$this->_body = json_decode( file_get_contents( 'php://input' ), true );
+				break;
+			case stristr( $this->contentType, 'application/x-www-form-urlencoded' ):
+				$this->_body = $_POST;
+				break;
+			case stristr( $this->contentType, 'text/plain' ):
+				$this->_body = file_get_contents( 'php://input' );
+				break;
+			default:
+				$this->_body = @file_get_contents( 'php://input' );
+				break;
+		} # end switch
+		
+	} # end constructor
+	
+	/**
+	 * @param string $key
+	 * @return array|mixed|null|string
+	 */
+	public function get( $key = '' ) {
+		if ( !is_array( $this->_body ) || empty($key) ) {
+			return isset($this->_body) ? $this->_body : null;
+		}
+		return isset($this->_body[$key]) ? $this->_body[$key] : null;
+	}
+	
+	/**
+	 * @param $key
+	 * @return null
+	 */
+	public function query( $key ) {
+		return isset($this->_query[$key]) ? $this->_query[$key] : null;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		return json_encode( $this );
+	}
+	
 } # end class
