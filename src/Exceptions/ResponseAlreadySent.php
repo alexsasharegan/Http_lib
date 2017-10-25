dd3194621ce92a4ba501c760e6bbc9c6e8ccc3c4
Alex Regan <alex.joseph.regan@gmail.com>
2017-10-25 13:35:16 -0700

    fix(http): new up req/res immediately in ctor; code format

--- src/Exceptions/ResponseAlreadySent.php
+++ src/Exceptions/ResponseAlreadySent.php
@@ -7,40 +7,36 @@ namespace Http\Exceptions;
  * @package Http\Exceptions
  */
 class ResponseAlreadySent extends \Exception implements \JsonSerializable {
-    
-    /**
-     * ResponseAlreadySent constructor.
-     *
-     * @param string          $message
-     * @param int             $code
-     * @param \Exception|NULL $previous
-     */
-    public function __construct( $message = '', $code = 0, \Exception $previous = NULL )
-    {
-        if ( empty( $message ) )
-        {
-            $message = "Response already sent. Cannot call send twice.";
-        }
-        parent::__construct( $message, $code, $previous );
+
+  /**
+   * ResponseAlreadySent constructor.
+   *
+   * @param string          $message
+   * @param int             $code
+   * @param \Exception|NULL $previous
+   */
+  public function __construct($message = '', $code = 0, \Exception $previous = NULL) {
+    if (empty($message)) {
+      $message = "Response already sent. Cannot call send twice.";
     }
-    
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return __CLASS__ . ". [code: {$this->code}] {$this->message}\n";
-    }
-    
-    /**
-     * @return array
-     */
-    public function jsonSerialize()
-    {
-        return [
-            'type'    => __CLASS__,
-            'message' => $this->message,
-            'code'    => $this->code,
-        ];
-    }
+    parent::__construct($message, $code, $previous);
+  }
+
+  /**
+   * @return string
+   */
+  public function __toString() {
+    return __CLASS__.". [code: {$this->code}] {$this->message}\n";
+  }
+
+  /**
+   * @return array
+   */
+  public function jsonSerialize() {
+    return [
+      'type' => __CLASS__,
+      'message' => $this->message,
+      'code' => $this->code,
+    ];
+  }
 }
